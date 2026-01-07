78067c8b2d7240915c705134ea6da66f9a0f25a9
Emmanuel-Adegboye <adetolaw3@gmail.com>
2026-01-07 16:03:28 +0100

    finished restaurant details

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -6,22 +6,22 @@ export interface Categories{
 
 export const categories : Categories[] = [
     {
-        id: 1, image: '/src/assets/images/categories/pizza.png', name: "Pizza",
+        id: 1, image: '/assets/images/categories/pizza.png', name: "Pizza",
     },
     {
-        id: 2, image: '/src/assets/images/categories/burger.png', name: "Burger"
+        id: 2, image: '/assets/images/categories/burger.png', name: "Burger"
     },
     {
-        id: 3, image: '/src/assets/images/categories/dessert.png', name: "Dessert"
+        id: 3, image: '/assets/images/categories/dessert.png', name: "Dessert"
     },
     {
-        id: 4, image: '/src/assets/images/categories/healthy.png', name: "Healthy"
+        id: 4, image: '/assets/images/categories/healthy.png', name: "Healthy"
     },
     {
-        id: 5, image: '/src/assets/images/categories/mexican.png', name: "Mexican"
+        id: 5, image: '/assets/images/categories/mexican.png', name: "Mexican"
     },
     {
-        id: 6, image: '/src/assets/images/categories/vegan.png', name: "Vegan"
+        id: 6, image: '/assets/images/categories/vegan.png', name: "Vegan"
     }
 ]
 
